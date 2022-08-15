9478f0995dab572c1737d6b211aa521d62ca286c
Vadim Fadeev <v.fadeev@velvetech.ru>
2022-08-15 10:03:02 +0400

    feat: fix build

--- compile_with_docker.sh
+++ compile_with_docker.sh
@@ -1,3 +1,3 @@
 git submodule init
 git submodule update
-docker run -v $(pwd)/.:/src dappsdevs/aptos-cli:0.2.5 aptos move compile --package-dir /src --named-addresses HelloBlockchain=$1 --output-dir /src/build
+docker run -v $(pwd)/.:/src dappsdevs/aptos-cli:0.2.5 aptos move compile --package-dir /src --named-addresses HelloBlockchain=$1 --output-dir /src
--- deploy/publish_hello_world.ts
+++ deploy/publish_hello_world.ts
@@ -9,9 +9,10 @@ const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
 
 async function main() {
   // Create two accounts, Alice and Bob, and fund Alice but not Bob
-  const publisher = new AptosAccount();
+  const publisherPrivateKey = Uint8Array.from(Buffer.from("d18aceadb73109bd36fe80fe0e51ba2f3d92a709a1325d5d89962842867e485c", 'hex'));
+  const publisher = new AptosAccount(publisherPrivateKey, "0xd3e84fde630a9803ea6e1dc89b1b587fa3efd3cf9817f6f360e59df0469a5fdc");
   await faucetClient.fundAccount(publisher.address(), 5_000);
-  const moduleHex = fs.readFileSync("./modules/hello_world.move").toString("hex");
+  const moduleHex = fs.readFileSync("./build/HelloBlockchain/bytecode_modules/Message.mv").toString("hex");
   console.log("module hex: ", moduleHex);
   const hashPublish = await publishModule(publisher, moduleHex);
   console.log("\n=== publish done ===");
