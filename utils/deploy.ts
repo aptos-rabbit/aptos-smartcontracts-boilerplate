1424bb320433933c851927d3eadc936b686512a4
Vadim Fadeev <v.fadeev@velvetech.ru>
2022-08-20 13:45:29 +0400

    add check status

--- utils/deploy.ts
+++ utils/deploy.ts
@@ -1,4 +1,6 @@
-import { publisherAccount } from "../config";
+import { Transaction } from "aptos/dist/generated/models/Transaction";
+import { Transaction_UserTransaction } from "aptos/dist/generated/models/Transaction_UserTransaction";
+import { faucetClient, publisherAccount } from "../config";
 import { compile, getNamedParametersFromToml } from "../libs/compile";
 import { publishModuleFromFile } from "../libs/deploy";
 
@@ -9,7 +11,18 @@ async function main() {
     for(const moduleName in compileRes.ByteCodeModulePaths) {
         const modulePath = compileRes.ByteCodeModulePaths[moduleName];
         const deployHash = await publishModuleFromFile(publisherAccount, modulePath);
-        console.log(`Module ${moduleName} was deployed; tx hash: ${deployHash}`);
+        const transaction = await faucetClient.getTransactionByHash(deployHash) as Transaction_UserTransaction;
+
+        if(transaction) {
+            if(transaction.success) {
+                console.log(`Module ${moduleName} was deployed; tx hash: ${deployHash}`);
+            } else {
+                console.log(`Module ${moduleName} wasn't deployed; tx hash: ${deployHash}`);
+                console.log(transaction.vm_status);
+            }
+            console.log(`https://explorer.devnet.aptos.dev/txn/${transaction.version}`);
+        }
+        
     }
 }
 
