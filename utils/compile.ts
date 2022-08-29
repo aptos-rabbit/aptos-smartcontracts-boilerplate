5a2595f473ea4a07ad366285d8e388b3ea7f1eb7
Dmitry Cherepanov <chrd717@gmail.com>
2022-08-29 08:36:56 +0300

    compile: iterate folders

--- utils/compile.ts
+++ utils/compile.ts
@@ -1,9 +1,32 @@
 import { compile, getNamedParametersFromToml } from "../libs/compile";
+import {promises as fs} from 'fs';
+import path from 'path';
 
 async function main() {
-    const addresses = await getNamedParametersFromToml();
-    const compileRes = await compile(addresses);
-    console.log(compileRes);
+    console.log('compile started');
+    var srcFolder = "./sources/src";
+
+    // Loop through all the files in the temp directory
+
+    try{
+        const folders = await fs.readdir(srcFolder);
+        console.log('projects count = ' + folders.length);
+        folders.forEach(async function (file, index) {
+            // Make one pass and make the file complete
+            var projectFolder = path.join(srcFolder, file);
+            console.log(projectFolder);
+            //const addresses = await getNamedParametersFromToml();
+            //const compileRes = await compile(addresses);
+            //console.log(compileRes);
+        });
+    }
+    catch(err){
+        console.error("Could not list the directory.", err);
+            process.exit(1);
+    }
+
+
+    console.log('compile finished');
 }
 
 main()
